a0cdaa0f6a4d22d4a2c5ab673b35a2305c9054cc
Hasmukh Sapariya <98575468+Hasmukh5414@users.noreply.github.com>
2023-01-30 17:28:59 +0530

    Add files via upload

---
+++ Lampros 1.py
@@ -0,0 +1,15 @@
+# def shuffle(nums, pos):
+#     aux = [None] * len(nums)
+#     for i, e in enumerate(pos):
+#         aux[e] = nums[i]
+#     for i in range(len(nums)):
+#         nums[i] = aux[i]
+# x1=[2,5,1,3,4,7]
+# p1=3
+# x2=[1,2,3,4,4,3,2,1]
+# p2=4
+# x3=[1,1,2,2]
+# p3=2
+# print(shuffle(x1,p1))
+# print(shuffle(x2,p2))
+# print(shuffle(x3,p3))
---
+++ Lampros2.py
@@ -0,0 +1,14 @@
+
+
+# def single_number(arr):
+#     result = 0
+#     for i in arr:
+#         result ^= i
+#     return result
+
+# arr1 = [2,2,1]
+# arr2=[4,1,2,1,2]
+# arr3=[1]
+# print(single_number(arr1))
+# print(single_number(arr2))
+# print(single_number(arr3))
